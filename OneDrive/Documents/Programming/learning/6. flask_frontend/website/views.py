71b7143da11e32c48f9909595b155e48d94d684d
Shivam <shivamprasad5953@gmail.com>
2025-12-11 22:03:10 +0530

    Delete OneDrive directory

--- OneDrive/Documents/Programming/learning/6. flask_frontend/website/views.py
+++
@@ -1,8 +0,0 @@
-# filepath: website/views.py
-from flask import Blueprint
-
-views = Blueprint('views', __name__)  # Define the Blueprint
-
-@views.route('/')
-def home():
-    return 'Welcome to the home page!'
